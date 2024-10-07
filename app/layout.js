a1cef9f3e0982c03d9df771f094f4d0db48a6837
Yousef Na <yousefnasr0541@gmail.com>
2024-10-08 02:16:19 +0300

    Empty-Commit

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,20 @@
+import MainHeader from '@/components/main-header';
 import './globals.css';
 
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
-}
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainHeader />
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
