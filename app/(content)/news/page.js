28ec4ddfb2ae516e615547ea3b18eed2b4b800fa
Yousef Na <yousefnasr0541@gmail.com>
2024-10-26 16:02:35 +0300

    add loading

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -2,7 +2,7 @@ import { NewsList } from '@/components';
 import { getAllNews } from '@/lib/news';
 
 export default async function NewsPage() {
-  const news = getAllNews();
+  const news = await getAllNews();
   return (
     <>
       <h1>News Page</h1>
